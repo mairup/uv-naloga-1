e7f8f8c9f2676a75bd2b2bbfad42d4a612cd2d41
mairup <maimairupnik@gmail.com>
2026-03-24 19:30:02 +0100

    fix for production build

--- build.gradle.kts
+++ build.gradle.kts
@@ -47,7 +47,7 @@ tasks.withType<Test> {
 }
 
 jlink {
-    imageZip.set(layout.buildDirectory.file("/distributions/app-${javafx.platform.classifier}.zip"))
+    imageZip.set(layout.buildDirectory.file("distributions/app-${javafx.platform.classifier}.zip"))
     options.set(listOf("--strip-debug", "--compress", "2", "--no-header-files", "--no-man-pages"))
     launcher {
         name = "app"
